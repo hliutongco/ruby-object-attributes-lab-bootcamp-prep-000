b7b6ec4fad50f05b32909b82a35f744d87253472
Helen Liutongco <bellpickle@gmail.com>
2017-12-19 03:17:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,8 @@
 class Dog
-  def initialize(name,breed)
-    @name = name
-    @breed = breed
+  def name
+    @name
   end
-  def name=(new_name)
-    @name = new_name
-  end
-  def breed=(new_breed)
-    @breed = new_breed
+  def breed
+    @breed
   end
 end
